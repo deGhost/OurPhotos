152d76aac1471be485546abc155fdac7191ca716
deGhost <sara7hamlili@gmail.com>
2021-05-18 02:10:15 +0200

    adding the photo upload form

---
+++ src/comps/UploadForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const UploadForm = () => {
+
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+    const types = ['image/png', 'image/jpg', 'image/jpeg'];
+
+    const changeHandler = (e) => {
+        // console.log("changed");
+        let selected = e.target.files[0];
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+            setError('')
+        } else {
+            setFile(null);
+            setError('Please select a valid file format: png or jpeg(jpg).');
+        }
+
+    }
+
+
+    return (
+        <form>
+            <input type="file" onChange={changeHandler} />
+            <div className="output">
+                {error && <div className="error">{error}</div>}
+                {file && <div> {file.name}</div>}
+            </div>
+        </form>
+    )
+}
+
+
+export default UploadForm;
